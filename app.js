a8025fb614398c5aa4e1fd9ad827ee33af317397
unknown <leandrojosezapata@gmail.com>
2023-09-19 20:07:16 -0300

    cargando archivos

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
+});
